ec9d19e365d1aa80ad481d415fd030a92209b79f
Tolmachev Mikhail <55889141+IceStormy1@users.noreply.github.com>
2023-11-23 20:26:12 +0300

    Add serilog

--- Authorization/Program.cs
+++ Authorization/Program.cs
@@ -2,6 +2,10 @@ using Authorization.Sql;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Events;
+using Serilog.Exceptions;
+using Serilog.Exceptions.Core;
 
 namespace Authorization;
 
@@ -18,9 +22,22 @@ public class Program
         Host.CreateDefaultBuilder(args)
             .ConfigureAppConfiguration((hostingContext, config) =>
             {
-                config.AddJsonFile("appsettings.json", false, true);
+                var env = hostingContext.HostingEnvironment;
+
+                config.AddJsonFile("appsettings.json", false, true)
+                    .AddJsonFile($"appsettings.{env.EnvironmentName}.json", true, true);
+
                 config.AddEnvironmentVariables();
             })
+            .UseSerilog((ctx, _, cfg) =>
+                cfg
+                    .Enrich.FromLogContext()
+                    .Enrich.WithExceptionDetails(new DestructuringOptionsBuilder())
+                    .Enrich.WithThreadId()
+                    .Enrich.WithThreadName()
+                    .ReadFrom.Configuration(ctx.Configuration)
+                    .MinimumLevel.Override("System", LogEventLevel.Information)
+            )
             .ConfigureWebHostDefaults(webBuilder =>
             {
                 webBuilder.UseStartup<Startup>();
