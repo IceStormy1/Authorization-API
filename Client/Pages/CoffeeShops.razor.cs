4cdc13483de16f666c3e443b152e446d280a0437
Tolmachev Mikhail <55889141+IceStormy1@users.noreply.github.com>
2023-11-23 16:42:21 +0300

    save comm

---
+++ Client/Pages/CoffeeShops.razor.cs
@@ -0,0 +1,37 @@
+﻿
+using Client.Services;
+using IdentityModel.Client;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Client.Models;
+
+
+namespace Client.Pages
+{
+    public partial class CoffeeShops
+    {
+        private List<CoffeeShopModel> Shops = new();
+        [Inject] private HttpClient HttpClient { get; set; }
+        [Inject] private IConfiguration Config { get; set; }
+        [Inject] private ITokenService TokenService { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            var tokenResponse = await TokenService.GetToken("TheaterApi.read");
+            HttpClient.SetBearerToken(tokenResponse.AccessToken);
+
+            //var result = await HttpClient.GetAsync(Config["apiUrl"] + "/api/CoffeeShop");
+
+            //if (result.IsSuccessStatusCode)
+            //{
+            //  Shops = await result.Content.ReadFromJsonAsync<List<CoffeeShopModel>>();
+            //}
+
+            Shops = new List<CoffeeShopModel>();
+        }
+    }
+}
---
+++ Client/Services/TokenService.cs
@@ -0,0 +1,44 @@
+﻿using IdentityModel.Client;
+using Microsoft.Extensions.Options;
+
+namespace Client.Services
+{
+    public class TokenService : ITokenService
+    {
+        public readonly IOptions<IdentityServerSettings> identityServerSettings;
+        private readonly ILogger<TokenService> logger;
+        public readonly DiscoveryDocumentResponse discoveryDocument;
+        private readonly HttpClient httpClient;
+
+        public TokenService(IOptions<IdentityServerSettings> identityServerSettings, ILogger<TokenService> logger)
+        {
+            this.identityServerSettings = identityServerSettings;
+            this.logger = logger;
+            httpClient = new HttpClient();
+            discoveryDocument = httpClient.GetDiscoveryDocumentAsync(this.identityServerSettings.Value.DiscoveryUrl).Result;
+
+            if (discoveryDocument.IsError)
+            {
+                throw new Exception("Unable to get discovery document", discoveryDocument.Exception);
+            }
+        }
+
+        public async Task<TokenResponse> GetToken(string scope)
+        {
+            var tokenResponse = await httpClient.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+            {
+                Address = discoveryDocument.TokenEndpoint,
+                ClientId = identityServerSettings.Value.ClientName,
+                ClientSecret = identityServerSettings.Value.ClientPassword,
+                Scope = scope
+            });
+
+            if (tokenResponse.IsError)
+            {
+                throw new Exception("Unable to get token", tokenResponse.Exception);
+            }
+            logger.LogInformation($"Bearer {tokenResponse.AccessToken}");
+            return tokenResponse;
+        }
+    }
+}
